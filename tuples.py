de8380e410971017778d27e0fa6cc5481fb79493
shrey <shreyyaps@gmail.com>
2026-01-27 12:38:22 +0530

    tuples as a record

---
+++ tuples.py
@@ -0,0 +1,10 @@
+lax_cordinates = (33.353456,-118.34645)
+city, year, pop, chg, area = ('Tokyo',2003, 32_450, 0.66, 8094)
+
+print(type(59_3))
+
+traveler_ids = [('USA', '31195855'), ('BRA','CE342567'),
+('ESP', 'XDA205856')]
+
+for country, num in sorted(traveler_ids):
+    print(country+"/"+num) # type: ignore
