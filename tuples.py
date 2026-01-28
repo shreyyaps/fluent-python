0c6f3eab175af782fc4b310dce4944c98e9c40d6
shrey <shreyyaps@gmail.com>
2026-01-28 12:42:01 +0530

    tuples as immutable lists

--- tuples.py
+++ tuples.py
@@ -1,3 +1,5 @@
+# tuples as records 
+
 lax_cordinates = (33.353456,-118.34645)
 city, year, pop, chg, area = ('Tokyo',2003, 32_450, 0.66, 8094)
 
@@ -8,3 +10,28 @@ traveler_ids = [('USA', '31195855'), ('BRA','CE342567'),
 
 for country, num in sorted(traveler_ids):
     print(country+"/"+num) # type: ignore
+
+
+
+# tuples as immutable lists
+
+a = (10,'shrey', [1,2])
+b = (10, 'shrey', [1, 2])
+b=a
+a[-1].append(99)
+print(a==b)
+
+
+def fixed(o):
+    try:
+        hash(o)
+    except TypeError:
+        return False
+    return True
+
+tf = ((1,2),)
+tm = ([1,2],)
+
+print(fixed(tf))
+print(fixed(tm))
+# page 34
