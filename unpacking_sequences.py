8d7aab643dac75d1553342bc29d60d0dc3a68b69
shrey <shreyyaps@gmail.com>
2026-01-30 19:28:51 +0530

    * unpacking operator

--- unpacking_sequences.py
+++ unpacking_sequences.py
@@ -1,6 +1,24 @@
-co_ordinates = (23.4354, -120.2443)
+import os
 
+co_ordinates = (23.4354, -120.2443)
 lat, long = co_ordinates
-a =1
-b=2
+a = 1
+b = 2
 a, b = b, a
+
+
+divmod(20, 8)
+
+t = (20, 8)
+
+divmod(*t)
+
+quotient, remainder = divmod(*t)
+
+
+
+path, filename = os.path.split('/home/luciano/.ssh/id_rs')
+
+print(path,"\n", filename)
+
+a, *packed, b, c = range(5)
